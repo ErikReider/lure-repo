4fd52cd6d9c5f679b0441e55a14e209bf46614c3
Erik Reider <erik.reider@protonmail.com>
2023-02-28 20:26:40 +0100

    Fixed swaylock permissions not being set correctly

--- swaylock-effects-erikreider-git/lure.sh
+++ swaylock-effects-erikreider-git/lure.sh
@@ -4,7 +4,7 @@ _pkgname=swaylock-effects
 name="$_pkgname-erikreider-git"
 _ver="1.6.10"
 version="$_ver-r241.b3a375a"
-release=1
+release=2
 desc="A fancier screen locker for Wayland."
 homepage="https://github.com/ErikReider/swaylock-effects"
 architectures=("amd64")
@@ -21,6 +21,12 @@ deps_fedora=("libxkbcommon-devel" "cairo-devel" "gdk-pixbuf2-devel" "pam-devel")
 sources=("git+https://github.com/ErikReider/swaylock-effects.git")
 checksums=("SKIP")
 
+scripts=(
+    ['postinstall']='postinstall.sh'
+    ['postupgrade']='postinstall.sh'
+    ['posttrans']='postinstall.sh'
+)
+
 version() {
     cd "$srcdir/$_pkgname"
     printf "$_ver-r%s.%s" "$(git rev-list --count HEAD)" "$(git rev-parse --short HEAD)"
@@ -34,5 +40,4 @@ build() {
 package() {
     cd "$srcdir"
     DESTDIR="$pkgdir" ninja -C build install
-    chmod a+s "$pkgdir/usr/bin/swaylock"
 }
---
+++ swaylock-effects-erikreider-git/postinstall.sh
@@ -0,0 +1,2 @@
+# Fix "swaylock needs to be setuid to read /etc/shadow"
+chmod a-s "/usr/bin/swaylock"
