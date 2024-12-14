cc28503a943514812e5b8de1f2f3aad92996299f
Erik Reider <35975961+ErikReider@users.noreply.github.com>
2024-12-15 00:48:53 +0100

    swaysettings-git: Bumped to v0.5.0

--- swaysettings-git/lure.sh
+++ swaysettings-git/lure.sh
@@ -2,8 +2,8 @@
 # vim: ft=sh
 name="swaysettings-git"
 _pkgname=swaysettings
-_ver="0.4.0"
-version="$_ver-r185.c3bb640"
+_ver="0.5.0"
+version="$_ver-r219.501052e"
 release=1
 desc="A gui for setting sway wallpaper, default apps, GTK themes, etc..."
 homepage="https://github.com/ErikReider/swaysettings"
@@ -12,13 +12,13 @@ license=("GPL3")
 provides=("swaysettings=$_ver")
 conflicts=("swaysettings")
 
-build_deps=("git" "meson" "scdoc" "vala")
-build_deps_fedora=("git" "meson" "vala" "gtk-layer-shell-devel" "libhandy-devel" "gtk3-devel" "glib2-devel" "libgee-devel" "gobject-introspection-devel" "json-glib-devel" "granite-devel" "libxml2-devel" "accountsservice-devel" "pulseaudio-libs-devel")
+build_deps=("git" "meson" "scdoc" "vala" "blueprint-compiler")
+build_deps_fedora=("git" "meson" "vala" "gtk4-layer-shell-devel" "libadwaita-devel" "gtk4-devel" "glib2-devel" "libgee-devel" "gobject-introspection-devel" "json-glib-devel" "granite-7-devel" "libxml2-devel" "accountsservice-devel" "pulseaudio-libs-devel" "libudisks2-devel" "libgtop2-devel")
 
-deps=("gtk3" "gtk-layer-shell" "libhandy" "glib2" "gobject-introspection" "libgee" "json-glib" "granite" "libxml2" "xkeyboard-config" "accountsservice" "gtk-layer-shell" "libpulse" "bluez")
-deps_fedora=("accountsservice" "gtk-layer-shell" "bluez" "pulseaudio-libs" "gtk3" "glib2")
+deps=("gtk4" "gtk4-layer-shell" "libadwaita" "glib2" "gobject-introspection" "libgee" "json-glib" "granite-7" "libxml2" "xkeyboard-config" "accountsservice" "gtk4-layer-shell" "libpulse" "bluez" "udisks2" "libgtop2")
+deps_fedora=("accountsservice" "gtk4-layer-shell" "bluez" "pulseaudio-libs" "gtk4" "glib2" "udisks2" "libgtop2")
 
-sources=("git+https://github.com/ErikReider/swaysettings.git")
+sources=("git+https://github.com/ErikReider/swaysettings.git?~rev=main")
 checksums=("SKIP")
 
 version() {
@@ -27,12 +27,12 @@ version() {
 }
 
 build() {
-    cd "$srcdir"
-    meson setup --prefix /usr "$_pkgname" build
+    cd "$srcdir/$_pkgname"
+    meson setup --prefix /usr build
     meson compile -C build
 }
 
 package() {
-    cd "$srcdir"
+    cd "$srcdir/$_pkgname"
     DESTDIR="$pkgdir" meson install -C build
 }
